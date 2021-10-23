8147dd79f9f9698930582a2ba45fe1298af7fbdc
sujishpradeep <sujishp164@gmail.com>
2021-10-23 20:34:07 +1100

    add test project

---
+++ BattleShipApiTests/Managers/GameManagerTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace BattleShipApiTests.Managers
+{
+    public class GameManagerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
