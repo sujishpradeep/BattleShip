02ccec6b4d1dd36e8ff88e996022e20d5cf60ee8
sujishpradeep <sujishp164@gmail.com>
2021-10-23 21:38:20 +1100

    add test for board already created

--- BattleShipApiTests/Managers/GameManagerTests.cs
+++ BattleShipApiTests/Managers/GameManagerTests.cs
@@ -2,25 +2,40 @@ using NUnit.Framework;
 using Moq;
 using BattleShipApi.Managers;
 using BattleShipApi.DataProcessing;
+using BattleShipApi.Models;
+using BattleShipApi.Constants;
+using System;
 
 namespace BattleShipApiTests.Managers
 {
     public class GameManagerTests
     {
-        Mock<IBoardDataProcessing> MockBoardDataProcessing;
+        Mock<IBoardDataProcessing> mockBoardDataProcessing;
         GameManager GameManager;
+        const int randomPlayerID = 111111;
+        const int randomGameID = 999999;
+
 
         [SetUp]
         public void Setup()
         {
-            MockBoardDataProcessing = new Mock<IBoardDataProcessing>();
-            GameManager = new GameManager(MockBoardDataProcessing.Object);
+            mockBoardDataProcessing = new Mock<IBoardDataProcessing>();
+            GameManager = new GameManager(mockBoardDataProcessing.Object);
         }
 
         [Test]
-        public void Test1()
+        public void AddBoard_Returns_Error_If_Board_Exists_For_Game_And_Player()
         {
-            Assert.Pass();
+            // Arrage
+            Color colorPreference = Color.Blue;
+            mockBoardDataProcessing.Setup(b => b.GetByGameIDAndPlayerID(randomGameID, randomPlayerID)).Returns(new Board());
+
+            // Act
+            var BoardResult = GameManager.AddBoard(randomGameID, randomPlayerID, colorPreference);
+
+            // Assert
+            Assert.IsTrue(BoardResult.IsError);
+            Assert.IsTrue(BoardResult.ErrorMessage.Contains("Board is already created for the player"));
         }
 
     }
