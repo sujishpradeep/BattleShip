28f5270c51059122613c75bbcc8ea46b7ef6ca09
sujishpradeep <sujishp164@gmail.com>
2021-10-24 17:53:22 +1100

    battle ship type validation

--- BattleShipApi/DTOs/BattleShipDTO.cs
+++ BattleShipApi/DTOs/BattleShipDTO.cs
@@ -1,11 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using BattleShipApi.Constants;
 using BattleShipApi.Models;
 
 namespace BattleShipApi.DTOs
 {
     public record BattleShipDTO
     {
-        public int BattleShipType { get; set; }
-        public int BattleShipAllignment { get; set; }
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [EnumDataType(typeof(BattleShipType), ErrorMessage = "Battleship type is invalid")]
+        [Required(ErrorMessage = "Battleship type is required")]
+        public BattleShipType? BattleShipType { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        [EnumDataType(typeof(BattleShipAllignment), ErrorMessage = "Battleship allignment is invalid")]
+
+        [Required(ErrorMessage = "Battleship allignment is required")]
+        public BattleShipAllignment? BattleShipAllignment { get; set; }
+
+        [Required(ErrorMessage = "Battleship starting cell is required")]
+
         public Cell StartingCell { get; set; }
     }
 }
