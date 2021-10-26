eb60c68612dd77becc8544b21cb42d24bfdb0eed
sujishpradeep <sujishp164@gmail.com>
2021-10-26 22:31:24 +1100

    test WillShipsOverlap

--- BattleShipApiTests/Managers/BattleShipManagerTests.cs
+++ BattleShipApiTests/Managers/BattleShipManagerTests.cs
@@ -35,6 +35,9 @@ namespace BattleShipApiTests.Managers
         {
 
             //Arrange 
+            // BattleShipWith known Strategy - Horizontal
+            // Input Strategy - (Unknown) Vertical
+
             var board = _fixture.Create<Board>();
             var battleShipType = _fixture.Create<BattleShipType>();
             var startingCell = _fixture.Create<Cell>();
@@ -46,17 +49,21 @@ namespace BattleShipApiTests.Managers
             var actual = battleShipManager.WillBoardOverFlowIfShipPlaced(board, battleShipType, battleShipAllignmentWithUnknownStrategy, startingCell);
 
             // Assert
-            Assert.IsFalse(actual);
+            // TODO: Should be exception
+            Assert.IsTrue(actual);
 
         }
         [Test]
         public void WillBoardOverFlowIfShipPlaced_Returns_Result_From_Strategy_When_Strategy_Is_Found()
         {
             //Arrange 
+            // BattleShipWith known Strategy - Horizontal
+            // Input Strategy - Horizontal
             var board = _fixture.Create<Board>();
             var battleShipType = _fixture.Create<BattleShipType>();
             var startingCell = _fixture.Create<Cell>();
             var battleShipAllignmentWithKnownStrategy = BattleShipAllignment.Horizontal;
+
             mockShipAllignmentStrategy.Setup(s => s.GetBattleShipAllignment()).Returns(battleShipAllignmentWithKnownStrategy);
             var expected = _fixture.Create<bool>();
             mockShipAllignmentStrategy.Setup(s => s.CheckIfBoardWillOverFlowWhenShipIsAdded(board, battleShipType, startingCell)).Returns(expected);
@@ -68,6 +75,73 @@ namespace BattleShipApiTests.Managers
             Assert.AreEqual(expected, actual);
 
         }
+
+        [Test]
+        public void WillShipsOverlap_Returns_True_If_Cell_Is_Already_Occupied()
+        {
+
+            //Arrange 
+
+            //Add 1 battleShip to board in Row 1, Col 1
+            var cells = new List<Cell> { new Cell(1, 1) };
+            var battleShips = new List<BattleShip>();
+            var battleShip = _fixture.Build<BattleShip>()
+                                      .With(b => b.CellsOccupied, cells)
+                                    .Create();
+            battleShips.Add(battleShip);
+            var boardState = _fixture.Build<BoardState>()
+                                      .With(b => b.BattleShips, battleShips)
+                                     .Create();
+
+            //Create a new battleShip in Row 1, Col 1
+            var cellsNew = new List<Cell> { new Cell(1, 1) };
+            var battleShipsNew = new List<BattleShip>();
+            var battleShipNew = _fixture.Build<BattleShip>()
+                                      .With(b => b.CellsOccupied, cellsNew)
+                                    .Create();
+
+
+            // Act 
+            var WillShipOverlap = battleShipManager.WillShipsOverlap(boardState, battleShipNew);
+
+            // Assert
+            Assert.IsTrue(WillShipOverlap);
+
+        }
+
+        [Test]
+        public void WillShipsOverlap_Returns_True_If_Cell_Is_Not_Already_Occupied()
+        {
+
+            //Arrange 
+
+            //Add 1 battleShip to board in Row 2, Col 2
+            var cells = new List<Cell> { new Cell(2, 2) };
+            var battleShips = new List<BattleShip>();
+            var battleShip = _fixture.Build<BattleShip>()
+                                      .With(b => b.CellsOccupied, cells)
+                                    .Create();
+            battleShips.Add(battleShip);
+            var boardState = _fixture.Build<BoardState>()
+                                      .With(b => b.BattleShips, battleShips)
+                                     .Create();
+
+            //Create a new battleShip in Row 1, Col 1
+            var cellsNew = new List<Cell> { new Cell(1, 1) };
+            var battleShipsNew = new List<BattleShip>();
+            var battleShipNew = _fixture.Build<BattleShip>()
+                                      .With(b => b.CellsOccupied, cellsNew)
+                                    .Create();
+
+
+            // Act 
+            var WillShipOverlap = battleShipManager.WillShipsOverlap(boardState, battleShipNew);
+
+            // Assert
+            Assert.IsFalse(WillShipOverlap);
+
+        }
+
 
     }
 }
